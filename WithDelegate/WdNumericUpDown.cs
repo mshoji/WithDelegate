c75b8cf6f92f5e19961d6b469359f1ae5f10247d
shoji <shoji@jwp.co.jp>
2017-06-28 08:54:47 +0900

    WithDelegate : NumericUpDownを追加した。

---
+++ WithDelegate/WdNumericUpDown.cs
@@ -0,0 +1,65 @@
+﻿/**
+WithDelegate
+
+Copyright (c) 2012 Masahiko SHOJI
+
+This software is released under the MIT License.
+
+http://opensource.org/licenses/mit-license.php
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WithDelegate
+{
+	public class WdNumericUpDown : System.Windows.Forms.NumericUpDown
+	{
+		////////////////////////////////////////////////////////////////
+		delegate void WdVisibleCallback(bool flg);
+		public void WdVisible(bool flg)
+		{
+			if (this.InvokeRequired)
+			{
+				WdVisibleCallback d = new WdVisibleCallback(WdVisible);
+				this.Invoke(d, new object[] { flg });
+			}
+			else
+			{
+				this.Visible = flg;
+			}
+		}
+		////////////////////////////////////////////////////////////////
+		delegate void WdEnableCallback(bool flg);
+		public void WdEnable(bool flg)
+		{
+			if (this.InvokeRequired)
+			{
+				WdEnableCallback d = new WdEnableCallback(WdEnable);
+				this.Invoke(d, new object[] { flg });
+			}
+			else
+			{
+				this.Enabled = flg;
+			}
+		}
+		////////////////////////////////////////////////////////////////
+		delegate void WdFocusCallback();
+		public void WdFocus()
+		{
+			if (this.InvokeRequired)
+			{
+				WdFocusCallback d = new WdFocusCallback(WdFocus);
+				this.Invoke(d, new object[] { });
+			}
+			else
+			{
+				this.Focus();
+			}
+		}
+		////////////////////////////////////////////////////////////////
+	}
+}
