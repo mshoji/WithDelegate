d87584a8bdb8edb81ab09b05674beb3c68f9f7cd
Masahiko SHOJI <shoji@jwp.co.jp>
2018-07-23 19:23:14 +0900

    version 2.14.0.0 : WdGroupBoxに対応した

---
+++ WithDelegate/WdGroupBox.cs
@@ -0,0 +1,107 @@
+﻿/**
+WithDelegate
+
+Copyright (c) 2012 Masahiko SHOJI
+
+This software is released under the MIT License.
+
+http://opensource.org/licenses/mit-license.php
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace WithDelegate
+{
+	public class WdGroupBox : System.Windows.Forms.GroupBox
+	{
+		///////////////////////////////////////////////////////////////////////
+		delegate void WdSetForeColorCallback(Color col);
+		public void WdSetForeColor(Color col)
+		{
+			if(this.InvokeRequired)
+			{
+				WdSetForeColorCallback d = new WdSetForeColorCallback(WdSetForeColor);
+				this.Invoke(d, new object[] { col });
+			}
+			else
+			{
+				this.ForeColor = col;
+			}
+		}
+		///////////////////////////////////////////////////////////////////////
+		delegate void WdSetBackColorCallback(Color col);
+		public void WdSetBackColor(Color col)
+		{
+			if(this.InvokeRequired)
+			{
+				WdSetBackColorCallback d = new WdSetBackColorCallback(WdSetBackColor);
+				this.Invoke(d, new object[] { col });
+			}
+			else
+			{
+				this.BackColor = col;
+			}
+		}
+		////////////////////////////////////////////////////////////////
+		delegate string WdGetTextCallback();
+		public string WdGetText()
+		{
+			if(this.InvokeRequired)
+			{
+				WdGetTextCallback d = new WdGetTextCallback(WdGetText);
+				return (string)this.Invoke(d, new object[] { });
+			}
+			else
+			{
+				return this.Text;
+			}
+		}
+		////////////////////////////////////////////////////////////////
+		delegate void WdSetTextCallback(string str);
+		public void WdSetText(string str)
+		{
+			if(this.InvokeRequired)
+			{
+				WdSetTextCallback d = new WdSetTextCallback(WdSetText);
+				this.Invoke(d, new object[] { str });
+			}
+			else
+			{
+				this.Text = str;
+			}
+		}
+		////////////////////////////////////////////////////////////////
+		delegate void WdVisibleCallback(bool flg);
+		public void WdVisible(bool flg)
+		{
+			if(this.InvokeRequired)
+			{
+				WdVisibleCallback d = new WdVisibleCallback(WdVisible);
+				this.Invoke(d, new object[] { flg });
+			}
+			else
+			{
+				this.Visible = flg;
+			}
+		}
+		////////////////////////////////////////////////////////////////
+		delegate void WdEnableCallback(bool flg);
+		public void WdEnable(bool flg)
+		{
+			if(this.InvokeRequired)
+			{
+				WdEnableCallback d = new WdEnableCallback(WdEnable);
+				this.Invoke(d, new object[] { flg });
+			}
+			else
+			{
+				this.Enabled = flg;
+			}
+		}
+	}
+}
